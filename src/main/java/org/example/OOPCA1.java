ed8e02162d23b1c50ce7d8b10493a44e746801c1
desmond madden <desmondmad19@gmail.com>
2020-10-22 00:00:03 +0100

    Second Commit selectFiveGrades() finished???

--- src/main/java/org/example/OOPCA1.java
+++ src/main/java/org/example/OOPCA1.java
@@ -1,71 +1,78 @@
 package org.example;
+
 import java.io.File;
-import java.util.Scanner;
-import java.util.Arrays;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class OOPCA1 {
     public static void main(String[] args) {
         Scanner scan = null;
         try {
-            String studentNum = null,
-                subjectCode1 = null,
-                subjectCode2 = null,
-                subjectCode3 = null,
-                subjectCode4 = null,
-                subjectCode5 = null,
-                subjectCode6 = null,
-                subjectCode7 = null,
-                subjectCode8 = null;
-
-            Double mark1 = null,
-                    mark2 = null,
-                    mark3 = null,
-                    mark4 = null,
-                    mark5 = null,
-                    mark6 = null,
-                    mark7 = null,
-                    mark8 = null;
+            String studentNum = null;
+            int subjectCode[] = new int[8];
+            int mark[] = new int[8];
+            int topMarks[] = null;
 
             scan = new Scanner(new File("JC_Results.txt"));
             scan.useDelimiter(",|\r\n");
 
             while(scan.hasNext())
             {
-                int studentResults[] = new int[17];
                 studentNum = scan.next();
-                subjectCode1 = scan.next();
-                mark1 = scan.nextDouble();
-                subjectCode2 = scan.next();
-                mark2 = scan.nextDouble();
-                subjectCode3 = scan.next();
-                mark3 = scan.nextDouble();
-                subjectCode4 = scan.next();
-                mark4 = scan.nextDouble();
-                subjectCode5 = scan.next();
-                mark5 = scan.nextDouble();
-                subjectCode6 = scan.next();
-                mark6 = scan.nextDouble();
-                subjectCode7 = scan.next();
-                mark7 = scan.nextDouble();
-                subjectCode8 = scan.next();
-                mark8 = scan.nextDouble();
+                System.out.println("Student Number: " + studentNum);
+                for(int i = 0; i < mark.length; i++){
+                    subjectCode[i] = scan.nextInt();
+                    mark[i] = scan.nextInt();
 
+                    System.out.println("Subject " + (i + 1) + ": " + subjectCode[i] + " Mark: " + mark[i] + "\n");
+                }
+                topMarks = selectFiveGrades(subjectCode, mark);
+                System.out.println(Arrays.toString(topMarks));
                 scan.nextLine();
-
-                System.out.println("Student Number: " + studentNum + "\nSubject 1: " + subjectCode1 + " Mark: " + mark1 + "\n"
-                        + "Subject 2: " + subjectCode2 + " Mark: " + mark2 + "\n"
-                        + "Subject 3: " + subjectCode3 + " Mark: " + mark3 + "\n"
-                        + "Subject 4: " + subjectCode4 + " Mark: " + mark4 + "\n"
-                        + "Subject 5: " + subjectCode5 + " Mark: " + mark5 + "\n"
-                        + "Subject 6: " + subjectCode6 + " Mark: " + mark6 + "\n"
-                        + "Subject 7: " + subjectCode7 + " Mark: " + mark7 + "\n"
-                        + "Subject 8: " + subjectCode8 + " Mark: " + mark8);
             }
+            scan.close();
 
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+    public static int[] selectFiveGrades(int[] codes, int[] grades){
+        int mandatoryMarks[] = new int[3];
+        int nonMandatory[] = new int[5];
+        int topMarks[] = new int[5];
+        int countM = 0;
+        int countN = 0;
+        for(int i = 0; i < grades.length;i++){
+            if(codes[i] == 1){
+                mandatoryMarks[countM] = grades[i];
+                countM++;
+            } else if(codes[i] == 2){
+                mandatoryMarks[countM] = grades[i];
+                countM++;
+            } else if(codes[i] == 3){
+                mandatoryMarks[countM] = grades[i];
+                countM++;
+            }
+            if(codes[i] != 1 && codes[i] != 2 && codes[i] != 3 && codes[i] != 218){
+                nonMandatory[countN] = grades[i];
+                countN++;
+            }
+        }
+        Arrays.sort(nonMandatory);
+        for(int i = 0; i < topMarks.length;i++){
+            if (i < 3){
+                topMarks[i] = mandatoryMarks[i];
+            }
+            else if(i == 3){
+                topMarks[i] = nonMandatory[nonMandatory.length - 2];
+            }
+            else{
+                topMarks[i] = nonMandatory[nonMandatory.length - 1];
+            }
+        }
+        return topMarks;
     }
 }
